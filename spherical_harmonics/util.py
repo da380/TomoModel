c6982363d756135bd3a71e788346dcbccea1c195
ij264 <ij264@cam.ac.uk>
2023-11-28 16:52:58 +0000

    import kernels integrate and plot DT on earth.

--- spherical_harmonics/util.py
+++ spherical_harmonics/util.py
@@ -1,5 +1,34 @@
 import numpy as np
 from  scipy.integrate import simpson
+import os
+
+def leading_zeros(degree):
+    return str(degree).zfill(3)
+
+
+def parse_file(path, number_of_columns=2):
+    with open(path, "r") as file:
+        # Initialize empty lists to store the data
+        radial_data = []
+        kernel_data = []
+
+        # Read and process each line
+        for line in file:
+            # Split the line into columns using space as the delimiter
+            columns = line.strip().split()
+
+            # Check if there are two columns
+            if len(columns) == 2:
+                try:
+                    # Convert the values to floats and append to respective lists
+                    radial_data.append(float(columns[0]))
+                    kernel_data.append(float(columns[1]))
+                except ValueError:
+                    print("Error: Invalid data format on line:", line)
+            else:
+                print("Error: Invalid data format on line:", line)
+    # convert to 2 dimensional numpy array where rows are kernel data and columns are radial data
+    return np.array(radial_data), np.flip(np.array(kernel_data))
 
 
 def preprocess(file_path):
@@ -86,20 +115,6 @@ def S20RTS_to_pyshtools(clm, sh_coeffs):
     return clm
 
 
-def simpson_integrate(y, x, axis=0):
-    """
-    Integrate a function y(x) using Simpson's rule.
-
-    :param np.ndarray y: 2D np array of y values.
-    :param np.ndarray x: 1D np array of x values.
-    :param int axis: Axis to integrate along.
-    :return np.ndarray integral: Array of integrated values along a specified axis.
-
-    """
-
-    integral = simpson(y, x, axis=axis)
-    return integral
-
 def integrate(l, density_sh_lm, radius_arr, kernel):
     """
 
@@ -116,8 +131,8 @@ def integrate(l, density_sh_lm, radius_arr, kernel):
     density_sh_l = density_sh_lm[:, :, l, :]
     # integrand is (radial_points x (2*l_max + 1))
     integrand = np.concatenate(
-                        (density_sh_l[:, 0, :] * kernel.reshape(-1, 1),
-                               density_sh_l[:, 1, :] * kernel.reshape(-1, 1)),
+                        (density_sh_l[:, 1, :] * kernel.reshape(-1, 1),
+                               density_sh_l[:, 0, :] * kernel.reshape(-1, 1)),
                                axis=1
                                )
 
